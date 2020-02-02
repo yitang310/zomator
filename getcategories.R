d7b0dd4c60e403d6e09aab021eff4b73e951b19d
yitang310 <tangyi2014@gmail.com>
2020-02-01 20:48:03 -0800

    update comments

--- getcategories.R
+++ getcategories.R
@@ -1,19 +1,20 @@
-#Enter parameter values
-#user-key: your API key
+#' @author Yi Tang <tangyi2014@@gmail.com>
+#'
+#' Enter parameter values
+#' @param user-key: your API key
+#'
+#' @return the data frame of category types
+#' category_id (integer): ID of the category type
+#' category_name (string): Name of the category type
+#'
+#' @export
+#' @examples
+#' get_categories(key="xxxxxx")
 
-#Return the data frame of category types
-#category_id (integer): ID of the category type 
-#category_name (string): Name of the category type
-
-#function for checking if api key is provided
-apikey_check <- function(key = NULL){
-  if (is.null(key)){
-    stop("Please provide a zomato API key.")
-  }
-}
 
 #function for getting categories
-get_categories <- function(key){
+get_categories <- function(key = NULL){
+  #check if given api
   apikey_check(key)
   baseurl <- "https://developers.zomato.com"
   urlcate <- httr::modify_url(baseurl, path = "/api/v2.1/categories")
@@ -21,13 +22,29 @@ get_categories <- function(key){
     url = urlcate,
     config = httr::add_headers("user-key" = key)
   )
+  #check if the api key can used to connect to zomato
+  apikey_connectioncheck(res)
+  #read json into dataframe
   datalist <- jsonlite::fromJSON(
     httr::content(
       res, as = "text", type = "application/json", encoding = "UTF-8"
     ),
     flatten = TRUE
   )
+  #only want the dataframe
   datalist <- datalist$categories
-  
   return(datalist)
+}
+
+#function for checking if api key is provided
+apikey_check <- function(key = NULL){
+  if (is.null(key)){
+    stop("Please provide a zomato API key.")
+  }
+}
+#function for checking if api can used to connect to zomato
+apikey_connectioncheck<-function(res = NULL){
+  if (!is.null(httr::content(res)$code)){
+    stop("please try another API key.")
+  }
 }
--- getcuisines.R
+++ getcuisines.R
@@ -1,36 +1,49 @@
-#Enter parameter values
-#user-key: your API key
-#city_id: id of the city for which collections are needed
-#lat: latitude
-#lon: longitude
+#' @author Yi Tang <tangyi2014@@gmail.com>
+#'
+#' Enter parameter values
+#' @param user-key: your API key
+#' @param city_id: id of the city for which collections are needed
+#' @param lat: latitude
+#' @param lon: longitude
 
-#Returns Zomato Restaurant Collections in a City
-#cuisine_id (integer): ID of the cuisine 
-#cuisine_name (string): Name of the cuisine
+#' @return Zomato Restaurant Collections in a City
+#' cuisine_id (integer): ID of the cuisine
+#' cuisine_name (string): Name of the cuisine
+#'
+#' @export
+#' @examples
+#' get_cuisines(key="xxxxxx")
 
 #function for finding all cuisines of restaurants listed in a city
-get_cuisines<-function(key,city_id,lat=NULL,lon=NULL){
+get_cuisines<-function(key=NULL,city_id=NULL,lat=NULL,lon=NULL){
   source("getcategories.R")
+  #check if given api
   apikey_check(key)
+  #check if given city_id
   if (is.null(city_id)){
     stop("Please enter the city ID. You can seach city ID by using get_cities function.")
   }
   baseurl <- "https://developers.zomato.com"
   urlcate <- httr::modify_url(baseurl, path = "/api/v2.1/cuisines")
   res <- httr::GET(
-    url = urlcate, 
+    url = urlcate,
     config = httr::add_headers("user-key" = key),
     query = list("city_id"=city_id,"lat"=lat,"lon"=lon)
   )
+  #check if the api key can used to connect to zomato
+  apikey_connectioncheck(res)
+  #read json into dataframe
   datalist <- jsonlite::fromJSON(
     httr::content(
       res, as = "text", type = "application/json", encoding = "UTF-8"
     ),
     flatten = TRUE
   )
+  #check if return results
   if (length(datalist$cuisines) == 0){
     stop("Please try another city ID.")
   }
+  #only want the dataframe
   datalist<-datalist$cuisines
   return(datalist)
 }
